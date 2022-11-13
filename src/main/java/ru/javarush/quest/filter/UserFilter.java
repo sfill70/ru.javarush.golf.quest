52ee4438972d9aaf2bc0e75490a7003f52f84292
sfill70 <sergfill70@yandex.ru>
2022-11-13 12:52:24 +0300

    edit name filter

--- src/main/java/ru/javarush/quest/filter/UserFilter.java
+++ src/main/java/ru/javarush/quest/filter/UserFilter.java
@@ -2,24 +2,20 @@ package ru.javarush.quest.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-@WebFilter(urlPatterns = "/start")
-public class XXSFilter implements Filter {
+@WebFilter/*(urlPatterns = "/start")*/
+public class UserFilter implements Filter {
 
-    public XXSFilter() {
+    public UserFilter() {
     }
 
     private FilterConfig filterConfig;
-    private static final Logger logger = LoggerFactory.getLogger(XXSFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserFilter.class);
 
     @Override
     public void init(FilterConfig fConfig) throws ServletException {
@@ -28,30 +24,29 @@ public class XXSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        logger.error("Filter!!!!!!!!!!!!!");
+        logger.error("Filter!");
         if (usernameCheck(req, resp)) {
             return;
         }
-//                chain.doFilter(new XSSRequestWrapper((HttpServletRequest) req), resp);
         chain.doFilter(req, resp);
     }
 
+    /*Здесь можно организовать валидацию "username"*/
     private boolean usernameCheck(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+        ServletContext ctx = filterConfig.getServletContext();
         if (req.getParameter("username").isBlank() || req.getParameter("username").isEmpty()) {
             req.setAttribute("blank", true);
-            ServletContext ctx = filterConfig.getServletContext();
             RequestDispatcher dispatcher = ctx.getRequestDispatcher("/");
             dispatcher.forward(req, resp);
             return true;
         }
-        if (req.getParameter("username").contains("*") || req.getParameter("username").contains("\\")) {
-            ServletContext ctx = filterConfig.getServletContext();
+        if (req.getParameter("username").contains("*") || req.getParameter("username").contains("\\")||
+                req.getParameter("username").contains("/")) {
+            req.setAttribute("blank", true);
             RequestDispatcher dispatcher = ctx.getRequestDispatcher("/");
             dispatcher.forward(req, resp);
             return true;
         }
         return false;
     }
-
-
 }
