173a40bb75e29d522f8cec518865aecfa5b042a1
sfill70 <sergfill70@yandex.ru>
2022-11-14 13:08:34 +0300

    edit Test

--- src/test/java/QuestServletTest.java
+++ src/test/java/QuestServletTest.java
@@ -1,16 +1,40 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.javarush.quest.QuestServlet;
+import ru.javarush.quest.factory.FactoryRepository;
+import ru.javarush.quest.repository.AnswerRepository;
+import ru.javarush.quest.repository.RepositoryEn;
+import ru.javarush.quest.repository.RepositoryRu;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.UnknownHostException;
+
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class QuestServletTest {
 
-  /*  public InitServlet initServlet;
+    public QuestServlet questServlet;
     public HttpServletRequest request;
     public HttpServletResponse response;
     public ServletContext servletContext;
     public RequestDispatcher requestDispatcher;
     public HttpSession currentSession;
-    public AnswerRepository answerRepository;
+    AnswerRepository answerRepository;
     FactoryRepository factoryRepository;
     int countLevel;
     String username;
@@ -25,14 +49,14 @@ public class QuestServletTest {
     String answer;
 
     private final String path = "/init-servlet";
-    private final String path2 = "/questquest.jsp";
+    private final String path2 = "/quest.jsp";
     private final String path3 = "/";
     String path4 = "/index.jsp";
 
     @BeforeEach
     public void init() throws ServletException {
-        this.initServlet = new InitServlet();
-        initServlet.init();
+        this.questServlet = new QuestServlet();
+        questServlet.init();
 //        initServlet.init();
 //        answerRepository = new RepositoryRu();
 //        this.factoryRepository = new FactoryRepository();
@@ -51,33 +75,32 @@ public class QuestServletTest {
             "RU, RepositoryRu.class",
             "EN, RepositoryEn.class",
     })
-    public void downloadDataByLanguageTest(String language) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-//        initServlet.downloadDataByLanguage(language);
-        Class clazz = initServlet.getClass();
-        Method downloadData = clazz.getDeclaredMethod("downloadDataByLanguage", String.class);
+    public void getAnswerRepositoryTest(String language) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Class clazz = questServlet.getClass();
+        Method downloadData = clazz.getDeclaredMethod("getAnswerRepository", String.class);
         downloadData.setAccessible(true);
-        downloadData.invoke(initServlet, language);
+        AnswerRepository repository = (AnswerRepository)downloadData.invoke(questServlet, language);
         if (language.equalsIgnoreCase("RU")) {
-            Assertions.assertEquals(initServlet.answerRepository.getClass(), RepositoryRu.class);
+            Assertions.assertEquals(repository.getClass(), RepositoryRu.class);
         } else {
-            Assertions.assertEquals(initServlet.answerRepository.getClass(), RepositoryEn.class);
+            Assertions.assertEquals(repository.getClass(), RepositoryEn.class);
         }
     }
 
     void dataTransferPerSessionTest(HttpServletRequest req) throws UnknownHostException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Class clazz = InitServlet.class;
+        Class clazz = QuestServlet.class;
         Method dataTransfer = clazz.getDeclaredMethod("dataTransferPerSession");
         dataTransfer.setAccessible(true);
         dataTransfer.invoke(req);
     }
 
 
-    @Test
- *//*   @ParameterizedTest
+   /* @Test
+    @ParameterizedTest
     @CsvSource({
             "/start?formname=prologue&username=svsv&choiceLanguage=RU, RepositoryRu.class",
-    })*//*
-
+    })*/
+/*
     public boolean startQuestTest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         when(request.getSession(true)).thenReturn(currentSession);
 //        when(request.getRequestURI()).thenReturn("/start");
@@ -86,19 +109,19 @@ public class QuestServletTest {
         when(request.getParameter("username")).thenReturn("user");
         System.out.println(request.getParameter("username") + "!!!!!!!!!!!!!!!");
         when(request.getParameter("choiceLanguage")).thenReturn("RU");
-        initServlet.startQuest(request, response);
+        questServlet.startQuest(request, response);
 //        initServlet.downloadDataByLanguage(language);
 //        Assertions.assertEquals(answerRepository.getClass(), );
-        Assertions.assertEquals(initServlet.answerRepository.getClass(), RepositoryRu.class);
+        Assertions.assertEquals(questServlet.answerRepository.getClass(), RepositoryRu.class);
         System.out.println(language);
 //        System.out.println(answerRepository.getWinMessage());
         return false;
-    }
+    }*/
 
-    @Test
-*//*    @ParameterizedTest
-       })*//*
-    public void PostTest(*//*String formName, String questionResponseBranch, String pathToJSP*//*) throws ServletException, IOException {
+/*    @Test
+    @ParameterizedTest
+       })
+    public void PostTest(String formName, String questionResponseBranch, String pathToJSP) throws ServletException, IOException {
         initServlet = new InitServlet();
         initServlet.init();
         request = mock(HttpServletRequest.class);
@@ -108,27 +131,27 @@ public class QuestServletTest {
         currentSession = mock(HttpSession.class);
         when(request.getSession(true)).thenReturn(currentSession);
 
-        *//* when(request.getParameter("formname")).thenReturn(String.valueOf("prologue"));*//*
+         when(request.getParameter("formname")).thenReturn(String.valueOf("prologue"));
         System.out.println(servletContext.getRequestDispatcher("/init-servlet"));
         System.out.println(servletContext.getRequestDispatcher("/mainPage"));
-      *//*  initServlet = new InitServlet();
+        initServlet = new InitServlet();
         request = mock(HttpServletRequest.class);
         servletContext = mock(ServletContext.class);
         currentSession = mock(HttpSession.class);
-        response = mock(HttpServletResponse.class);*//*
+        response = mock(HttpServletResponse.class);
 //        currentSession.setAttribute("formname", "prologue");
         System.out.println(currentSession.getAttribute("formname") + "!!!!!!!!!!!!!!!");
 
-*//*
+
         System.out.println(request.getParameter("formname"));
         System.out.println(request.getContextPath());
         System.out.println(request.getParameter("username"));
         System.out.println(servletContext.getContext("formname"));
         System.out.println(request.getParameter("choiceLanguage"));
-        System.out.println(request);*//*
-    }
+        System.out.println(request);
+    }*/
 
-    @Test
+/*    @Test
     public void doPostTest() throws ServletException, IOException, URISyntaxException {
         initServlet = new InitServlet();
         initServlet.init();
