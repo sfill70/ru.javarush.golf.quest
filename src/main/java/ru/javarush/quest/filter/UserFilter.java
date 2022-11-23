82adc83095b31318d4871fc4edc499441d370370
sfill70 <sergfill70@yandex.ru>
2022-11-23 17:04:52 +0300

    edit filter

--- src/main/java/ru/javarush/quest/filter/UserFilter.java
+++ src/main/java/ru/javarush/quest/filter/UserFilter.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-@WebFilter/*(urlPatterns = "/start")*/
+@WebFilter(urlPatterns = "/start")
 public class UserFilter implements Filter {
 
     public UserFilter() {
